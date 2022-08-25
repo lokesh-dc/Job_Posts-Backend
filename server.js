01ca9abc8e2a0b57a4d361e350dfb6ca9d4904bc
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-08-25 16:51:36 +0530

    correcting

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const jsonServer = require('json-server');
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
