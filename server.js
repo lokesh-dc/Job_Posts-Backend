8f5f4ff8bb91f419e65a38f4a31ef768376a6f64
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-08-25 16:32:10 +0530

    server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 5000;
 const path=require("path");
 const jsonServer=require("json-server");
 const server=jsonServer.create();
-const router=jsonServer.router(path.join(__dirname,"./db.json"));
+const router=jsonServer.router(path.join(__dirname,"db.json"));
 const middlewares=jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
